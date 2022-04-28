1ae2839d34174a4327c360136027894f965cdcd8
ansel brandt <ansel@anselbrandt.com>
2022-04-28 00:38:54 -0400

    Fetch data from product-api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    getData();
+    async function getData() {
+      const result = await fetch("http://localhost:9090/products");
+      const json = await result.json();
+      setData(json);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : "No Data"}
     </div>
   );
 }
