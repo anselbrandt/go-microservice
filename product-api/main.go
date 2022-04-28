4cbdaddc0aebca8f89be5507c1025dec35a47cab
ansel brandt <ansel@anselbrandt.com>
2022-04-28 00:59:01 -0400

    Add CORS handler

--- product-api/main.go
+++ product-api/main.go
@@ -10,10 +10,12 @@ import (
 
 	"github.com/go-openapi/runtime/middleware"
 
-	"github.com/gorilla/mux"
 	"product-api/data"
-	"product-api/handlers"
 	"product-api/env"
+	"product-api/handlers"
+
+	gohandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
@@ -54,10 +56,12 @@ func main() {
 	getR.Handle("/docs", sh)
 	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
+
 	// create a new server
 	s := http.Server{
 		Addr:         *bindAddress,      // configure the bind address
-		Handler:      sm,                // set the default handler
+		Handler:      ch(sm),            // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
